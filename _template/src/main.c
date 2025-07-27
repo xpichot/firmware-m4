53649cb22e37895c6a83230f2c189cf68d269379
xpichot <xavpich7@gmail.com>
2025-07-27 09:58:11 +0200

    Add complete configuration of a m4 firmware. Template projects builds correctly.

---
+++ _template/src/main.c
@@ -0,0 +1,251 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "ipcc.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "virt_uart.h"
+#include "openamp_log.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+VIRT_UART_HandleTypeDef huart0, huart1;
+
+__IO FlagStatus VirtUart0RxMsg = RESET;
+char mUartBuffTx[512];
+
+uint8_t VirtUart0ChannelBuffRx[100];
+uint16_t VirtUart0ChannelRxSize = 0;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+/**
+  * @brief  CallBack function which will be called when UART0 data is received from A7.
+  * @retval none
+  */
+void VIRT_UART0_RxCpltCallback(VIRT_UART_HandleTypeDef *huart)
+{
+    // copy received msg in a buffer
+    VirtUart0ChannelRxSize = huart->RxXferSize < 100? huart->RxXferSize : 99;
+    memcpy(VirtUart0ChannelBuffRx, huart->pRxBuffPtr, VirtUart0ChannelRxSize);
+    VirtUart0ChannelBuffRx[VirtUart0ChannelRxSize] = 0;   // insure end of String
+#if TRC_LA
+    sprintf(mUartBuffTx1, "CM4 : VIRT_UART0_RxCpltCallback: %s\n", VirtUart0ChannelBuffRx);
+    VIRT_UART_Transmit(&huart1, (uint8_t*)mUartBuffTx1, strlen(mUartBuffTx1));
+#endif
+    VirtUart0RxMsg = SET;
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+  __HAL_RCC_HSEM_CLK_ENABLE();
+
+  // Major difference with MCU, state of resources is not known => HW reset before initialization
+//  resetPeripherals();
+  /* USER CODE END Init */
+
+  if(IS_ENGINEERING_BOOT_MODE())
+  {
+    /* Configure the system clock */
+    SystemClock_Config();
+  }
+  else
+  {
+    /* IPCC initialisation */
+    MX_IPCC_Init();
+    /* OpenAmp initialisation ---------------------------------*/
+  }
+  MX_OPENAMP_Init(RPMSG_REMOTE, NULL);
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  /* USER CODE BEGIN 2 */
+
+  /*
+   * Create Virtual UART devices
+   */
+  //log_info("Virtual UART0 OpenAMP-rpmsg channel creation\r\n");
+  if (VIRT_UART_Init(&huart0) != VIRT_UART_OK) {
+    //log_err("VIRT_UART_Init UART0 failed.\r\n");
+    Error_Handler();
+  }
+
+  //log_info("Virtual UART1 OpenAMP-rpmsg channel creation\r\n");
+  if (VIRT_UART_Init(&huart1) != VIRT_UART_OK) {
+    //log_err("VIRT_UART_Init UART1 failed.\r\n");
+    Error_Handler();
+  }
+
+  /*Need to register callback for message reception by channels (not for UART1 as used only for trace outpout) */
+  //if(VIRT_UART_RegisterCallback(&huart0, VIRT_UART_RXCPLT_CB_ID, VIRT_UART0_RxCpltCallback) != VIRT_UART_OK)
+  //{
+  //  Error_Handler();
+  //}
+
+  mUartBuffTx[0] = 60;
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+	  //HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_1);
+      //VIRT_UART_Transmit(&huart0, (uint8_t*)mUartBuffTx, 1);
+      VIRT_UART_Transmit(&huart0, (uint8_t*)"Hello from M4\n", strlen("Hello from M4\n"));
+      mUartBuffTx[0]++;
+	  HAL_Delay(1);
+
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_LSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSIDivValue = RCC_HSI_DIV1;
+  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL2.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL3.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL4.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** RCC Clock Config
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_ACLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2
+                              |RCC_CLOCKTYPE_PCLK3|RCC_CLOCKTYPE_PCLK4
+                              |RCC_CLOCKTYPE_PCLK5;
+  RCC_ClkInitStruct.AXISSInit.AXI_Clock = RCC_AXISSOURCE_HSI;
+  RCC_ClkInitStruct.AXISSInit.AXI_Div = RCC_AXI_DIV1;
+  RCC_ClkInitStruct.MCUInit.MCU_Clock = RCC_MCUSSOURCE_HSI;
+  RCC_ClkInitStruct.MCUInit.MCU_Div = RCC_MCU_DIV1;
+  RCC_ClkInitStruct.APB4_Div = RCC_APB4_DIV1;
+  RCC_ClkInitStruct.APB5_Div = RCC_APB5_DIV1;
+  RCC_ClkInitStruct.APB1_Div = RCC_APB1_DIV1;
+  RCC_ClkInitStruct.APB2_Div = RCC_APB2_DIV1;
+  RCC_ClkInitStruct.APB3_Div = RCC_APB3_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
